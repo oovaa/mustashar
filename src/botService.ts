77a4ba8d0fd9b5c0652cc4e46b5523dd65d7d295
Amar-dev1 <3marofficial249@gmail.com>
2026-01-20 13:51:06 +0200

    created the summary logic

--- src/botService.ts
+++ src/botService.ts
@@ -1,23 +1,43 @@
 import { Context } from "hono";
 import { getLLM } from "./llm";
+import { neon } from "@neondatabase/serverless";
 
 const botService = async (c: Context) => {
+  const sql = neon(c.env.DATABASE_URL);
+
   try {
     const update = await c.req.json();
     const chat_id = update.message?.chat.id;
     const userText = update.message?.text;
 
     if (chat_id && userText) {
-      const llm = getLLM(c.env.GROQ_API_KEY);
-      const aiResponse = await llm.invoke(userText);
-      const aiAnswer = aiResponse.content;
+      // 1. Iinitlize 2 llms
+      const summarizerLLM = getLLM(c.env.GROQ_API_KEY);
+      const assistanceLLM = getLLM(c.env.GROQ_API_KEY);
 
+      //2. getting the stored summary
+      const result =
+        await sql`SELECT summary FROM user_memories WHERE chat_id = ${chat_id}`;
+      const oldSummary = result[0]?.summary || "No history found";
+
+      //3. updating the old summary
+      const updatedSummary = await summarizerLLM.invoke(
+        `Update the summary. Old: ${oldSummary}. new message: ${userText}.`,
+      );
+      await sql`INSERT INTO user_memories (chat_id, summary) VALUES (${chat_id}, ${updatedSummary}) ON CONFLICT (chat_id) DO UPDATE summary = ${userText}`;
+
+      // 4. generating the final answer
+      const finalAnswer = await assistanceLLM.invoke(
+        `- the past messages's summary: ${updatedSummary}\n - the comming user message: ${userText}`,
+      );
+
+      // 5. sending back the answer to the bot
       await fetch(
         `https://api.telegram.org/bot${c.env.BOT_TOKEN}/sendMessage`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ chat_id: chat_id, text: aiAnswer }),
+          body: JSON.stringify({ chat_id: chat_id, text: finalAnswer.content }),
         },
       );
     }
