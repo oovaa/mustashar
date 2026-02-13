c96f5058d7d9e90ea7f46683a96b70a40b3abe6f
Asma Elhadi <asmaa.alhai100.gmail.com>
2026-02-13 15:03:27 +0300

    changing the chunk size and overlap

--- src/rag/chunker.ts
+++ src/rag/chunker.ts
@@ -10,8 +10,8 @@ export async function loadDocsFromFolder(
 ): Promise<Document[]> {
   const docs: Document[] = []
   const splitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 800,
-    chunkOverlap: 0,
+    chunkSize: 900,
+    chunkOverlap:100,
   })
 
   // Recursively find all .pdf, .txt, .md files
